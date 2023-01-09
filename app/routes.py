5052675f2175fe4740dc044c2aa9e20bd63beb09
unknown <tylermiller1321@outlook.com>
2023-01-09 16:59:52 -0600

    flask intro

---
+++ app/routes.py
@@ -0,0 +1,11 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+def index():
+    fruits = ['apple', 'banana', 'kiwi', 'strawberry', 'watermelon', 'pineapple']
+    return render_template('index.html', name='Tyler', fruits=fruits)
+    
+@app.route('/posts')
+def posts():
+    return 'these are the post'
