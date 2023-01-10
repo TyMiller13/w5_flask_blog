4ef01b98296f49f476d3c8a54db847cdce62f72a
unknown <tylermiller1321@outlook.com>
2023-01-10 15:54:03 -0600

    added alerts and forms files

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for, flash
+from app.forms import SignUpForm
+
 
 @app.route('/')
 def index():
@@ -10,6 +12,27 @@ def index():
 def posts():
     return 'these are the post'
 
-@app.route('/signup')
+@app.route('/signup', methods = ["GET", "POST"])
 def signup():
-    return render_template('signup.html')
+    #create an instance of the SignUpForm
+    form = SignUpForm()
+    #Check if a POST request AND data is valid
+    if form.validate_on_submit():
+        print('Form Submitted and Validated!')
+        # Get the data from the form
+        email = form.email.data
+        username = form.username.data
+        password = form.password.data
+        print(email, username, password)
+        # TODO: Check to see if there is a User with the username in the database
+        if username == 'Ty.Codes':
+            flash('That user already exists', 'danger')
+            return redirect(url_for('signup'))
+        # TODO: Create a new User with form data and add to database
+        # Flash a success message
+        flash('Thank you for signing up!', 'success')
+        # redirect back to home
+        return redirect(url_for('index'))
+
+    return render_template('signup.html', form = form)
+
