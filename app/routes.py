ba7097fdea943beb98418df4a58206683acf4053
unknown <tylermiller1321@outlook.com>
2023-01-11 16:23:56 -0600

    debug condition

--- app/routes.py
+++ app/routes.py
@@ -6,18 +6,21 @@ from app.models import User
 
 @app.route('/')
 def index():
-    fruits = ['apple', 'banana', 'kiwi', 'strawberry', 'watermelon', 'pineapple']
+    fruits = ['apple', 'banana', 'kiwi',
+              'strawberry', 'watermelon', 'pineapple']
     return render_template('index.html', name='Tyler', fruits=fruits)
-    
+
+
 @app.route('/posts')
 def posts():
     return 'these are the post'
 
-@app.route('/signup', methods = ["GET", "POST"])
+
+@app.route('/signup', methods=["GET", "POST"])
 def signup():
-    #create an instance of the SignUpForm
+    # create an instance of the SignUpForm
     form = SignUpForm()
-    #Check if a POST request AND data is valid
+    # Check if a POST request AND data is valid
     if form.validate_on_submit():
         print('Form Submitted and Validated!')
         # Get the data from the form
@@ -26,11 +29,12 @@ def signup():
         password = form.password.data
         print(email, username, password)
        # Query our user table to see if there are any users with either username or email from form
-        check_user = User.query.filter( (User.username == username ) |(User.email == email ) ).all()
+        check_user = User.query.filter(
+            (User.username == username) | (User.email == email)).all()
         # If the query comes back with any results
-        if username == check_user:
-            #Flash message saying that a user with email/username already exists
-            flash('A user with that email and/ or username already exists.', 'danger')
+        if check_user:
+            # Flash message saying that a user with email/username already exists
+            flash('A user with that email and/or username already exists.', 'danger')
             return redirect(url_for('signup'))
         # If check_user is empty, creater a new record in the user table
         new_user = User(email=email, username=username, password=password)
@@ -39,5 +43,4 @@ def signup():
         # redirect back to home
         return redirect(url_for('index'))
 
-    return render_template('signup.html', form = form)
-
+    return render_template('signup.html', form=form)
