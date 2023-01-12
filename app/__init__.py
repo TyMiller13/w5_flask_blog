95cc9c448c6e2f65a134e716c77967896f3c3618
unknown <tylermiller1321@outlook.com>
2023-01-12 12:32:38 -0600

    working on login

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 #Import SQLAlchemy and Migrate from their modules
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 #Import the Config class from the config module - will have all the apps configurations
 from config import Config
 
@@ -15,7 +16,8 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 #Create an instance of Migrate to represent out migration engine
 migrate = Migrate(app, db)
-
+#Create an instance of LoginManager to setup login functionality
+login = LoginManager(app)
 
 #import all of the routes from the routes file into the current folder
 from . import routes, models
