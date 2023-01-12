efb7f257ccfbcecb33780f0ab0b1ce209c50b34b
unknown <tylermiller1321@outlook.com>
2023-01-12 14:32:13 -0600

    created log in and logout

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template, redirect, url_for, flash
+from flask_login import login_user, logout_user
 from app.forms import SignUpForm, LoginForm
 from app.models import User
 
@@ -48,4 +49,26 @@ def signup():
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        #get the username and password from form
+        username = form.username.data
+        password = form.password.data
+        print(username, password)
+        # Query the user table to check if there is a user with that username
+        user = User.query.filter_by(username=username).first()
+        #Check if there is a user and that the password is correct
+        if user is not None and user.check_password(password):
+            #log the user in
+            login_user(user)
+            flash(f"{user.username} is now logged in", "warning")
+            return redirect(url_for('index'))
+        else:
+            flash(f"Incorrect username and/or password", "danger")
+            return redirect(url_for('login'))
     return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    flash(f"You have been logged out", "warning")
+    return redirect(url_for('index'))
